3dcce75144961cbf07a8318097e74dc63eaa62a2
Monkt <Monkt@192.168.1.47>
2025-04-27 11:02:58 +0530

    Built a Spring Boot REST API with endpoints:   - `/hello`: Returns "Hello, Spring Boot!".   - `/tasks`: Returns all tasks as JSON.   - `/tasks/pending`: Filters pending tasks using streams.

---
+++ src/main/java/com/example/model/Task.java
@@ -0,0 +1,20 @@
+package com.example.model;
+
+public class Task {
+	 private int id;
+	    private String name;
+	    private String status;
+	    private int  timeRequired;
+	    public Task(int id, String name, String status, int timeRequired) {
+	        this.id = id;
+	        this.name = name;
+	        this.status = status;
+	        this.timeRequired=timeRequired;
+	        
+	    }
+	    // Getters
+	    public int getId() { return id; }
+	    public String getName() { return name; }
+	    public String getStatus() { return status; }
+	    public int getTimeRequired() {return timeRequired;}
+}
---
+++ src/main/java/com/example/task_api/TaskController.java
@@ -0,0 +1,32 @@
+package com.example.task_api;
+
+
+
+import com.example.model.Task;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@RestController
+public class TaskController {
+    // Hardcoded task list
+    private List<Task> tasks = Arrays.asList(
+        new Task(1, "Code", "pending",12),
+        new Task(2, "Test", "done",11),
+        new Task(3, "Deploy", "pending",6)
+    );
+
+    @GetMapping("/tasks")
+    public List<Task> getTasks() {
+        return tasks;
+    }
+
+    @GetMapping("/tasks/pending")
+    public List<Task> getPendingTasks() {
+        return tasks.stream()
+            .filter(t -> t.getStatus().equals("pending"))
+            .collect(Collectors.toList());
+    }
+}
